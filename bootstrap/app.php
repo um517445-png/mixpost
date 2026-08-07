c0101b15e5e9ba5b660ad238004764fa4d6a33d9
um517445-png <um517445@gmail.com>
2026-08-07 22:14:47 +0300

    fix(bootstrap): register core Laravel ViewServiceProvider and SessionServiceProvider in bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,6 +6,15 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 $app = Application::configure(basePath: dirname(__DIR__))
     ->withProviders([
+        \Illuminate\View\ViewServiceProvider::class,
+        \Illuminate\Session\SessionServiceProvider::class,
+        \Illuminate\Cache\CacheServiceProvider::class,
+        \Illuminate\Filesystem\FilesystemServiceProvider::class,
+        \Illuminate\Database\DatabaseServiceProvider::class,
+        \Illuminate\Translation\TranslationServiceProvider::class,
+        \Illuminate\Validation\ValidationServiceProvider::class,
+        \Illuminate\Cookie\CookieServiceProvider::class,
+        \Illuminate\Routing\RoutingServiceProvider::class,
         \Inovector\Mixpost\MixpostServiceProvider::class,
     ])
     ->withMiddleware(function (Middleware $middleware) {
