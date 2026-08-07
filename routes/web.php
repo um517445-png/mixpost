bf874f81f3e6c124c0e059f1af5d11565c9a2664
um517445-png <um517445@gmail.com>
2026-08-07 23:14:27 +0300

    fix(routes): add full Mixpost route group with web middleware pipeline

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inovector\Mixpost\Http\Controllers\AccountsController;
+use Inovector\Mixpost\Http\Controllers\CallbackSocialProviderController;
+use Inovector\Mixpost\Http\Controllers\DashboardController;
+use Inovector\Mixpost\Http\Controllers\DeleteSocialProviderController;
+use Inovector\Mixpost\Http\Controllers\PostAccountsController;
+use Inovector\Mixpost\Http\Controllers\PostsController;
+use Inovector\Mixpost\Http\Controllers\ServicesController;
+use Inovector\Mixpost\Http\Controllers\SettingsController;
+use Inovector\Mixpost\Http\Middleware\Auth;
+use Inovector\Mixpost\Http\Middleware\HandleInertiaRequests;
 
 Route::get('/', function () {
     return redirect('/mixpost');
@@ -9,3 +19,27 @@ Route::get('/', function () {
 Route::get('/login', function () {
     return redirect('/mixpost');
 })->name('login');
+
+Route::prefix(config('mixpost.prefix', 'mixpost'))
+    ->name('mixpost.')
+    ->middleware([
+        'web',
+        Auth::class,
+        HandleInertiaRequests::class,
+    ])
+    ->group(function () {
+        Route::get('/', DashboardController::class)->name('dashboard');
+        Route::get('/services', [ServicesController::class, 'index'])->name('services.index');
+        Route::post('/services', [ServicesController::class, 'update'])->name('services.update');
+        Route::get('/accounts', [AccountsController::class, 'index'])->name('accounts.index');
+        Route::post('/accounts/callback/{provider}', CallbackSocialProviderController::class)->name('accounts.callback');
+        Route::delete('/accounts/{provider}', DeleteSocialProviderController::class)->name('accounts.delete');
+        Route::get('/posts', [PostsController::class, 'index'])->name('posts.index');
+        Route::get('/posts/create', [PostsController::class, 'create'])->name('posts.create');
+        Route::post('/posts', [PostsController::class, 'store'])->name('posts.store');
+        Route::get('/posts/{post}/edit', [PostsController::class, 'edit'])->name('posts.edit');
+        Route::put('/posts/{post}', [PostsController::class, 'update'])->name('posts.update');
+        Route::delete('/posts/{post}', [PostsController::class, 'destroy'])->name('posts.destroy');
+        Route::get('/settings', [SettingsController::class, 'index'])->name('settings.index');
+        Route::put('/settings', [SettingsController::class, 'update'])->name('settings.update');
+    });
