fffd14085086e4242bc788a72a36825e29f4dded
um517445-png <um517445@gmail.com>
2026-08-07 22:19:24 +0300

    fix(bootstrap): configure Laravel Application withRouting and withProviders for full container initialization

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,38 +1,24 @@
 <?php
 
 use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
 
-$app = new Application(dirname(__DIR__));
+$app = Application::configure(basePath: dirname(__DIR__))
+    ->withProviders([
+        \Inovector\Mixpost\MixpostServiceProvider::class,
+    ])
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+    )
+    ->withMiddleware(function (Middleware $middleware) {
+        //
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })
+    ->create();
 
 $app->useStoragePath('/tmp');
-
-$app->singleton(
-    Illuminate\Contracts\Http\Kernel::class,
-    Illuminate\Foundation\Http\Kernel::class
-);
-
-$app->singleton(
-    Illuminate\Contracts\Console\Kernel::class,
-    Illuminate\Foundation\Console\Kernel::class
-);
-
-$app->singleton(
-    Illuminate\Contracts\Debug\ExceptionHandler::class,
-    Illuminate\Foundation\Exceptions\Handler::class
-);
-
-$app->register(Illuminate\Events\EventServiceProvider::class);
-$app->register(Illuminate\Log\LogServiceProvider::class);
-$app->register(Illuminate\Routing\RoutingServiceProvider::class);
-$app->register(Illuminate\Filesystem\FilesystemServiceProvider::class);
-$app->register(Illuminate\View\ViewServiceProvider::class);
-$app->register(Illuminate\Session\SessionServiceProvider::class);
-$app->register(Illuminate\Cache\CacheServiceProvider::class);
-$app->register(Illuminate\Database\DatabaseServiceProvider::class);
-$app->register(Illuminate\Translation\TranslationServiceProvider::class);
-$app->register(Illuminate\Validation\ValidationServiceProvider::class);
-$app->register(Illuminate\Cookie\CookieServiceProvider::class);
-
-$app->register(Inovector\Mixpost\MixpostServiceProvider::class);
 
 return $app;
