d93abfc74f41d648648ccda665548c5d43fbf4a5
um517445-png <um517445@gmail.com>
2026-08-07 23:09:06 +0300

    fix(middleware): append StartSession and HandleInertiaRequests middleware in bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,7 +19,13 @@ $app = Application::configure(basePath: dirname(__DIR__))
         web: __DIR__.'/../routes/web.php',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->web(append: [
+            \Illuminate\Cookie\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \Inovector\Mixpost\Http\Middleware\HandleInertiaRequests::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
