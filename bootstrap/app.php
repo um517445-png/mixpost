c0a9f284fc1e39e049366d6d7275103760385fc0
um517445-png <um517445@gmail.com>
2026-08-07 22:11:59 +0300

    fix(bootstrap): bind ExceptionHandler and Middleware in bootstrap/app.php for Laravel 11/12 container initialization

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,17 @@
 <?php
 
 use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withProviders([
         \Inovector\Mixpost\MixpostServiceProvider::class,
     ])
+    ->withMiddleware(function (Middleware $middleware) {
+        //
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })
     ->create();
